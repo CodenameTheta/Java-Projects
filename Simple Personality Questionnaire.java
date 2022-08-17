129af2a5b9ffd468c3809fd0d6829290d2dd53cf
CodenameTheta <71943908+CodenameTheta@users.noreply.github.com>
2022-08-17 13:59:55 -0400

    Update Simple Personality Questionnaire.java

--- Simple Personality Questionnaire.java
+++ Simple Personality Questionnaire.java
@@ -34,17 +34,23 @@ public class CodingCauseImBored {
         System.out.println("            Welcome to something to decide who you are!!!           ");
         System.out.println("====================================================================\n");
 
+        int dummyInt = 1;
+        var checkInt = (((Object)dummyInt).getClass().getSimpleName());
+        int firstAnswer = scanObj.nextInt();
+        var checkAnswer = (((Object)firstAnswer).getClass().getSimpleName());
+        
         System.out.print("how much is your annual salary?:  ");
         Scanner scanObj = new Scanner(System.in);
-        int firstAnswer = scanObj.nextInt();
-
+            
+        
         System.out.println("");
         System.out.println("what is your life strategy?: ");
+        
         System.out.println("1.) working for a company\n 2.) Entrepenuer\n 3.) Cocaine runner");
         int secondAnswer = scanObj.nextInt();
 
         scanObj.close();
-
+        
         
         if((firstAnswer < 30000)&&(secondAnswer == 3)){
             System.out.println("you are: " + attributeListOne[randAttOne] + " " + titleListOne[randTitleOne]);
@@ -52,8 +58,20 @@ public class CodingCauseImBored {
             System.out.println("you are: " + attributeListTwo[randAttTwo] + " " + titleListOne[randTitleOne]);
         }else if((firstAnswer > 70000)&&(secondAnswer == 3)){
             System.out.println("you are: " + attributeListThree[randAttThree] + " " + titleListOne[randTitleOne]);
-        }else if((firstAnswer < 30000)&&(secondAnswer == 2)){
+        }else if((firstAnswer < 30000)&&(secondAnswer == 1)){
             System.out.println("you are: " + attributeListOne[randAttOne] + " " + titleListTwo[randTitleTwo]);
+        }else if((firstAnswer > 30000)&&(firstAnswer < 70000)&&(secondAnswer == 1)){
+            System.out.println("you are: " + attributeListTwo[randAttTwo] + " " + titleListTwo[randTitleTwo]);
+        }else if((firstAnswer > 70000)&&(secondAnswer == 1)){
+            System.out.println("you are: " + attributeListThree[randAttThree] + " " + titleListTwo[randTitleTwo]);
+        }else if((firstAnswer < 30000)&&(secondAnswer == 2)){
+            System.out.println("you are: " + attributeListOne[randAttOne] + " " + titleListThree[randTitleThree]);
+        }else if((firstAnswer > 30000)&&(firstAnswer < 70000)&&(secondAnswer == 2)){
+            System.out.println("you are: " + attributeListTwo[randAttTwo] + " " + titleListThree[randTitleThree]);
+        }else if((firstAnswer > 70000)&&(secondAnswer == 2)){
+            System.out.println("you are: " + attributeListThree[randAttThree] + " " + titleListThree[randTitleThree]);
+        }else{
+            System.out.println("WHY IS IT NOT WORKING???");
         }
 
     }
