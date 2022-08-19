cdef65916833ae95a3e4d70a52fcbb2cde6f197b
CodenameTheta <71943908+CodenameTheta@users.noreply.github.com>
2022-08-18 21:13:05 -0400

    Update Simple Personality Questionnaire.java

--- Simple Personality Questionnaire.java
+++ Simple Personality Questionnaire.java
@@ -2,46 +2,56 @@ import java.util.Scanner;
 
 
 public class CodingCauseImBored {
+	
+    String[] attributeListOne = {"low class", "empoverished", "sad", "struggling", "paycheck-to-paycheck"};
+    String[] attributeListTwo = {"average", "middle of the road", "normal", "somewhat stable", "plain Jane"};
+    String[] attributeListThree = {"high class", "high value", "gold status", "disirable", "sought after"};
+
+    String[] titleListOne = {"trash", "loser", "dead-beat", "broke"};
+    String[] titleListTwo = {"normie", "cog in the machine", "office worker", "Joe"};
+    String[] titleListThree = {"elite", "aristicrat", "high earner", "one percenter"};
+
+    int attListIndexOne = attributeListOne.length;
+    int attListIndexTwo = attributeListTwo.length;
+    int attListIndexThree = attributeListThree.length;
+
+    int titleListIndexOne = titleListOne.length;
+    int titleListIndexTwo = titleListTwo.length;
+    int titleListIndexThree = titleListThree.length;
+
+    java.util.Random randomGenerator = new java.util.Random();
+
+    int randAttOne = randomGenerator.nextInt(attListIndexOne);
+    int randAttTwo = randomGenerator.nextInt(attListIndexTwo);
+    int randAttThree = randomGenerator.nextInt(attListIndexThree);
+
+    int randTitleOne = randomGenerator.nextInt(titleListIndexOne);
+    int randTitleTwo = randomGenerator.nextInt(titleListIndexTwo);
+    int randTitleThree = randomGenerator.nextInt(titleListIndexThree);
+    
+    int dummyInt = 1;
+    var checkInt = (((Object)dummyInt).getClass().getSimpleName());
+    int firstAnswer = scanObj.nextInt();
+    var checkAnswer = (((Object)firstAnswer).getClass().getSimpleName());
+    
+    static void firstResponse() {
+    	do {
+    		System.out.print("how much is your annual salary?:  ");
+            Scanner scanObj = new Scanner(System.in);
+            if(checkAnswer == checkInt) {
+            	continue;
+            } else {
+            	System.out.println("put in a numerical value!!");
+            } while(checkAnswer != checkInt);
+    	}
+    }
+	
     public static void main(String[] args){
         
-        String[] attributeListOne = {"low class", "empoverished", "sad", "struggling", "paycheck-to-paycheck"};
-        String[] attributeListTwo = {"average", "middle of the road", "normal", "somewhat stable", "plain Jane"};
-        String[] attributeListThree = {"high class", "high value", "gold status", "disirable", "sought after"};
-
-        String[] titleListOne = {"trash", "loser", "dead-beat", "broke"};
-        String[] titleListTwo = {"normie", "cog in the machine", "office worker", "Joe"};
-        String[] titleListThree = {"elite", "aristicrat", "high earner", "one percenter"};
-
-        int attListIndexOne = attributeListOne.length;
-        int attListIndexTwo = attributeListTwo.length;
-        int attListIndexThree = attributeListThree.length;
-
-        int titleListIndexOne = titleListOne.length;
-        int titleListIndexTwo = titleListTwo.length;
-        int titleListIndexThree = titleListThree.length;
-
-        java.util.Random randomGenerator = new java.util.Random();
-
-        int randAttOne = randomGenerator.nextInt(attListIndexOne);
-        int randAttTwo = randomGenerator.nextInt(attListIndexTwo);
-        int randAttThree = randomGenerator.nextInt(attListIndexThree);
-
-        int randTitleOne = randomGenerator.nextInt(titleListIndexOne);
-        int randTitleTwo = randomGenerator.nextInt(titleListIndexTwo);
-        int randTitleThree = randomGenerator.nextInt(titleListIndexThree);
-
-
-        System.out.println("            Welcome to something to decide who you are!!!           ");
+    	System.out.println("            Welcome to something to decide who you are!!!           ");
         System.out.println("====================================================================\n");
 
-        int dummyInt = 1;
-        var checkInt = (((Object)dummyInt).getClass().getSimpleName());
-        int firstAnswer = scanObj.nextInt();
-        var checkAnswer = (((Object)firstAnswer).getClass().getSimpleName());
-        
-        System.out.print("how much is your annual salary?:  ");
-        Scanner scanObj = new Scanner(System.in);
-            
+        firstResponse();
         
         System.out.println("");
         System.out.println("what is your life strategy?: ");
@@ -49,8 +59,6 @@ public class CodingCauseImBored {
         System.out.println("1.) working for a company\n 2.) Entrepenuer\n 3.) Cocaine runner");
         int secondAnswer = scanObj.nextInt();
 
-        scanObj.close();
-        
         
         if((firstAnswer < 30000)&&(secondAnswer == 3)){
             System.out.println("you are: " + attributeListOne[randAttOne] + " " + titleListOne[randTitleOne]);
