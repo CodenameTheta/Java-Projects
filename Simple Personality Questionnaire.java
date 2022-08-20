c53768a5e6ccce4fac20685bf686b977b6d9265f
CodenameTheta <71943908+CodenameTheta@users.noreply.github.com>
2022-08-20 15:30:54 -0400

    Update Simple Personality Questionnaire.java

--- Simple Personality Questionnaire.java
+++ Simple Personality Questionnaire.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 
 public class CodingCauseImBored {
+
+    static CodingCauseImBored classObj = new CodingCauseImBored();
 	
     String[] attributeListOne = {"low class", "empoverished", "sad", "struggling", "paycheck-to-paycheck"};
     String[] attributeListTwo = {"average", "middle of the road", "normal", "somewhat stable", "plain Jane"};
@@ -29,58 +31,53 @@ public class CodingCauseImBored {
     int randTitleTwo = randomGenerator.nextInt(titleListIndexTwo);
     int randTitleThree = randomGenerator.nextInt(titleListIndexThree);
     
-    int dummyInt = 1;
-    var checkInt = (((Object)dummyInt).getClass().getSimpleName());
-    int firstAnswer = scanObj.nextInt();
-    var checkAnswer = (((Object)firstAnswer).getClass().getSimpleName());
-    
-    static void firstResponse() {
-    	do {
-    		System.out.print("how much is your annual salary?:  ");
-            Scanner scanObj = new Scanner(System.in);
-            if(checkAnswer == checkInt) {
-            	continue;
-            } else {
-            	System.out.println("put in a numerical value!!");
-            } while(checkAnswer != checkInt);
-    	}
+    Scanner scanObj = new Scanner(System.in);
+  
+    public void makeYourChoice() {
+
+        try {
+            System.out.print("what is your anual salary?: ");
+            int firstAnswer = scanObj.nextInt();
+            System.out.println("");
+            System.out.println("what is your life strategy?: ");
+            
+            System.out.println("1.) working for a company\n 2.) Entrepenuer\n 3.) Cocaine runner");
+            int secondAnswer = scanObj.nextInt();
+
+            if((firstAnswer < 30000)&&(secondAnswer == 3)){
+                System.out.println("you are: " + attributeListOne[randAttOne] + " " + titleListOne[randTitleOne]);
+            }else if(((firstAnswer > 30000))&&(firstAnswer < 70000)&&(secondAnswer == 3)){
+                System.out.println("you are: " + attributeListTwo[randAttTwo] + " " + titleListOne[randTitleOne]);
+            }else if((firstAnswer > 70000)&&(secondAnswer == 3)){
+                System.out.println("you are: " + attributeListThree[randAttThree] + " " + titleListOne[randTitleOne]);
+            }else if((firstAnswer < 30000)&&(secondAnswer == 1)){
+                System.out.println("you are: " + attributeListOne[randAttOne] + " " + titleListTwo[randTitleTwo]);
+            }else if((firstAnswer > 30000)&&(firstAnswer < 70000)&&(secondAnswer == 1)){
+                System.out.println("you are: " + attributeListTwo[randAttTwo] + " " + titleListTwo[randTitleTwo]);
+            }else if((firstAnswer > 70000)&&(secondAnswer == 1)){
+                System.out.println("you are: " + attributeListThree[randAttThree] + " " + titleListTwo[randTitleTwo]);
+            }else if((firstAnswer < 30000)&&(secondAnswer == 2)){
+                System.out.println("you are: " + attributeListOne[randAttOne] + " " + titleListThree[randTitleThree]);
+            }else if((firstAnswer > 30000)&&(firstAnswer < 70000)&&(secondAnswer == 2)){
+                System.out.println("you are: " + attributeListTwo[randAttTwo] + " " + titleListThree[randTitleThree]);
+            }else if((firstAnswer > 70000)&&(secondAnswer == 2)){
+                System.out.println("you are: " + attributeListThree[randAttThree] + " " + titleListThree[randTitleThree]);
+            }else{
+                System.out.println("WHY IS IT NOT FUCKING WORKING???");
+            } 
+        } catch(Exception e){
+            System.out.println("you didn't input an integer value!");
+            System.exit(0);
+        } 
+
     }
+
 	
     public static void main(String[] args){
         
     	System.out.println("            Welcome to something to decide who you are!!!           ");
         System.out.println("====================================================================\n");
 
-        firstResponse();
-        
-        System.out.println("");
-        System.out.println("what is your life strategy?: ");
-        
-        System.out.println("1.) working for a company\n 2.) Entrepenuer\n 3.) Cocaine runner");
-        int secondAnswer = scanObj.nextInt();
-
-        
-        if((firstAnswer < 30000)&&(secondAnswer == 3)){
-            System.out.println("you are: " + attributeListOne[randAttOne] + " " + titleListOne[randTitleOne]);
-        }else if(((firstAnswer > 30000))&&(firstAnswer < 70000)&&(secondAnswer == 3)){
-            System.out.println("you are: " + attributeListTwo[randAttTwo] + " " + titleListOne[randTitleOne]);
-        }else if((firstAnswer > 70000)&&(secondAnswer == 3)){
-            System.out.println("you are: " + attributeListThree[randAttThree] + " " + titleListOne[randTitleOne]);
-        }else if((firstAnswer < 30000)&&(secondAnswer == 1)){
-            System.out.println("you are: " + attributeListOne[randAttOne] + " " + titleListTwo[randTitleTwo]);
-        }else if((firstAnswer > 30000)&&(firstAnswer < 70000)&&(secondAnswer == 1)){
-            System.out.println("you are: " + attributeListTwo[randAttTwo] + " " + titleListTwo[randTitleTwo]);
-        }else if((firstAnswer > 70000)&&(secondAnswer == 1)){
-            System.out.println("you are: " + attributeListThree[randAttThree] + " " + titleListTwo[randTitleTwo]);
-        }else if((firstAnswer < 30000)&&(secondAnswer == 2)){
-            System.out.println("you are: " + attributeListOne[randAttOne] + " " + titleListThree[randTitleThree]);
-        }else if((firstAnswer > 30000)&&(firstAnswer < 70000)&&(secondAnswer == 2)){
-            System.out.println("you are: " + attributeListTwo[randAttTwo] + " " + titleListThree[randTitleThree]);
-        }else if((firstAnswer > 70000)&&(secondAnswer == 2)){
-            System.out.println("you are: " + attributeListThree[randAttThree] + " " + titleListThree[randTitleThree]);
-        }else{
-            System.out.println("WHY IS IT NOT WORKING???");
-        }
-
+        classObj.makeYourChoice();
     }
 }
